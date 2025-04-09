b0e2ffbca00be13e9eb071f5290a6c0465483186
Excavator <kvronin@gmail.com>
2025-04-09 23:01:03 +0500

    front_cars: first commit

---
+++ front_cars/src/App.jsx
@@ -0,0 +1,17 @@
+import { } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex h-screen justify-center items-center'>
+        <h1 className="text-3xl font-bold text-violet-600 ">
+          Hello Cars!
+        </h1>
+      </div>      
+    </>
+  )
+}
+
+export default App
